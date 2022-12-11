497435d4994f3867388657b40615e3c174a9ec95
Sergii Polushkin <seregapola@gmail.com>
2022-12-11 22:40:33 +0100

    fix: add api news link

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.up.railway.app/', {
             apiKey: 'c79f2add35184ce18175f5f4bfd3da6f', // получите свой ключ https://newsapi.org/
         });
     }
